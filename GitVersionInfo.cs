49927b1e0d180b55b8f769de0c1eac5e84581281
robert vanderluit <rvanderluit@acs-west.com>
2026-05-01 21:36:21 -0700

    5-1-26 git version after compile changed

--- GitVersionInfo.cs
+++ GitVersionInfo.cs
@@ -2,7 +2,7 @@ namespace ACS_4Series_Template_V3
 {
     public static class GitVersionInfo
     {
-        public const string CommitHash = "385d9d2";
+        public const string CommitHash = "006e1dc";
         public const string Branch = "main";
         public const string CommitDate = "2026-04-30";
     }
