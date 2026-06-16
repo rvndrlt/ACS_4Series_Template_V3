4ead4b71c35072ec78fabe08cb1cddba4b8b5d44
robert vanderluit <rvanderluit@acs-west.com>
2026-06-15 17:13:08 -0700

    6-15-26 new page flips

--- GitVersionInfo.cs
+++ GitVersionInfo.cs
@@ -2,8 +2,8 @@ namespace ACS_4Series_Template_V3
 {
     public static class GitVersionInfo
     {
-        public const string CommitHash = "4c8c875";
-        public const string Branch = "diagnostics-mem-leak";
-        public const string CommitDate = "2026-06-13";
+        public const string CommitHash = "b6a8a83";
+        public const string Branch = "feature/json-page-flip-descriptor";
+        public const string CommitDate = "2026-06-14";
     }
 }
